7564fad8e2ed4a0ca9f3489d4f13b4542226c70c
HyekangPark <hyekang.park@yonsei.ac.kr>
2023-12-08 07:18:19 +0000

    fix

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="calibrate",
+    version="0.1",
+    packages=find_packages(),
+    python_requries=">=3.8",
+    install_requires=[
+        "torch",
+        "torchvision>=0.8.2",
+        "ipdb==0.13.9",
+        "albumentations==1.1.0",
+        "opencv-python==4.5.1.48",
+        "hydra-core==1.2.0",
+        "flake8==4.0.1",
+        "wandb==0.12.14",
+        "terminaltables==3.1.10",
+        "matplotlib==3.5.1",
+        "plotly==5.7.0",
+        "pandas==1.4.2"
+    ],
+)
